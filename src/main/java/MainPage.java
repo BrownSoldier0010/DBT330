78dcdabf45f3e3d4a1b725e2b169228520c981b5
Unknown <isaiahcjc5@gmail.com>
2018-08-21 17:46:01 -0600

    Added Outline and spring collection

---
+++ src/main/java/MainPage.java
@@ -0,0 +1,19 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+public class MainPage {
+
+    public TextArea NameBox;
+
+    public TextArea Summary;
+
+    public TextArea BirthBox;
+
+    public Label NameLabel;
+
+    public TextArea Othertext;
+
+
+
+}
---
+++ src/main/java/SelectPage.java
@@ -0,0 +1,12 @@
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+
+public class SelectPage {
+
+    public ListView SelectPage;
+
+    public void ChoosePage(MouseEvent mouseEvent) {
+
+    }
+
+}
